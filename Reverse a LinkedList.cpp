620a90fd9d1d48655d6a7ba47f3c8f8b9670b696
Anubhav Sharma <57109153+anubhvshrma18@users.noreply.github.com>
2024-07-07 17:47:54 +0530

    Added Iterative Solution

--- Reverse a LinkedList.cpp
+++ Reverse a LinkedList.cpp
@@ -1,1 +1,15 @@
-
+// Leetcode - 
+// Iterative Solution
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr=head,*prev=NULL,*nex=NULL;
+        while(curr){
+            nex=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nex;
+        }
+        return prev;
+    }
+};
