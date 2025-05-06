01a6e490dcadeabeb60ca2876080a780468e9a0f
anubhvshrma18 <57109153+anubhvshrma18@users.noreply.github.com>
2025-05-06 22:45:23 +0530

    Day 4

--- Day-004/Finding the Length of Loop in LL.cpp
+++ Day-004/Finding the Length of Loop in LL.cpp
@@ -1,1 +1,45 @@
 
+/*
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+    int countNodes(Node* node) {
+        int res = 1;
+        Node* curr = node;
+        while (curr->next != node) {
+            res++;
+            curr = curr->next;
+        }
+        return res;
+    }
+  public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+        // Code here
+        Node *slow = head, *fast = head;
+
+        while (slow != nullptr && fast != nullptr 
+               && fast->next != nullptr) {
+                   
+            slow = slow->next;
+            fast = fast->next->next;
+    
+            // If slow and fast meet at
+            // some point then there is a loop
+            if (slow == fast)
+                return countNodes(slow);
+        }
+    
+        // Return 0 to indicate that
+        //   there is no loop
+        return 0;
+    }
+};
