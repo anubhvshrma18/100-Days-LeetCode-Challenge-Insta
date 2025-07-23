2f9feccc87b9159e3a660fa943651e54606d27e2
anubhvshrma18 <57109153+anubhvshrma18@users.noreply.github.com>
2025-07-24 03:33:09 +0530

    Day 45

--- Day-045/Convert Sorted Array to Binary Search Tree.cpp
+++ Day-045/Convert Sorted Array to Binary Search Tree.cpp
@@ -5,15 +5,17 @@
 // Time Complexity: O(n)
 // Space Complexity: O(n) for the recursion stack
 
-// Definition for a binary tree node
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
 class Solution {
 private:
